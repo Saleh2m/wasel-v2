4e9c4fdb9e5d8f25d2b6a11c5cc4b233ad598dab
Saleh2m <ipadx48@gmail.com>
2026-04-23 14:27:54 +0300

    Update supabase.ts

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 export const supabase = createClient(
   'https://aufvtaabhgqealykhhid.supabase.co',
-  'sb_publishable_8LtaairnPw0WRaCMstlnGQ_wfDsuLJwe0F'
-);
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1ZnZ0YWFiaGdxZWFseWtoaGlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NDExODgsImV4cCI6MjA5MjQxNzE4OH0.kNg1IkSJ-MbNEc0OuX18yvu6tN31miAzJqzmE5WBiM0'
+)
